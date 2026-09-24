18288ed41f0c456447fc6fa12eda6872660a35fb
ESLAMYASSER-1 <eng.eslam.yasser.1@gmail.com>
2026-09-24 15:29:51 +0300

    edit the base router and requirements

--- run_csab.sh
+++ run_csab.sh
@@ -2,26 +2,28 @@
 set -e
 
 echo "Starting Docker services..."
-cd ~/CSAB-APP/docker
+cd docker
 docker compose up -d
 
 echo "Installing Python dependencies..."
-cd ~/CSAB-APP
+cd ../ 
+source .venv3.10/bin/activate 
+
 pip install -r requirements.txt
 
 echo "Fixing cryptography / GLIBC issue..."
 pip uninstall cryptography -y
 pip install "cryptography<42"
 
-echo "Setting HuggingFace cache..."
-export HF_HOME=~/cache
+#echo "Setting HuggingFace cache..."
+#export HF_HOME=~/cache
 
 echo "Initializing MongoDB..."
-docker exec -i docker-mongo-1 mongosh -u admin -p admin123 <<EOF
-use CSAP_DB
-db.ADMIN.insertOne({Name: "eslam", Password: "eslam"})
+docker exec -i csab-mongo-1 mongosh -u admin -p admin123 <<EOF
+use CSAB_DB
+db.ADMIN.insertOne({Role: "admin", Name: "eslam", Password: "eslam"})
 EOF
 
 echo "Starting FastAPI app..."
-cd ~/CSAB-APP/src
+cd src
 uvicorn main:app --host 0.0.0.0 --port 5000
--- src/routes/base.py
+++ src/routes/base.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 baseRouter = APIRouter(tags=["base"])
 
 
+
 @baseRouter.get("/")
 async def FirstLanding():
     return {"message": "Welcome To CSAB APP"}
